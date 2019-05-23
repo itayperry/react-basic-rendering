b8ead109485036202073df3339309792b07e7c48
itay <itayperry91@gmail.com>
2019-05-23 15:11:11 +0300

    Updates

---
+++ precache-manifest.a1b5935ab444386d13ee600d12cc1266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c674e79f976d391bb8c176e3011a09",
+    "url": "/react-basic-rendering/index.html"
+  },
+  {
+    "revision": "01ee3ef2aecdeafb2575",
+    "url": "/react-basic-rendering/static/css/main.1aff83e6.chunk.css"
+  },
+  {
+    "revision": "379c5c18559fb414093b",
+    "url": "/react-basic-rendering/static/js/2.cf759ac7.chunk.js"
+  },
+  {
+    "revision": "01ee3ef2aecdeafb2575",
+    "url": "/react-basic-rendering/static/js/main.d455246e.chunk.js"
+  },
+  {
+    "revision": "12f106fd57381f936ae6",
+    "url": "/react-basic-rendering/static/js/runtime~main.9c17e657.js"
+  }
+]);
